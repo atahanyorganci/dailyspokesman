db10519a46f54a3ad8f8ede6dd7f115d23c0a827
Atahan Yorgancı <atahanyorganci@hotmail.com>
2020-01-09 02:19:36 +0300

    add custom error pages

--- newsapp/routes.py
+++ newsapp/routes.py
@@ -58,3 +58,31 @@ def news(page, number):
             except:
                 break
     abort(404)
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    desc = {
+        'title': '404 Not Found',
+        'description': 'Tthe server can\'t find the requested resource.',
+        'fix': 'Please check your request\'s URL.'
+    }
+    return render_template('error.html', error=desc), 404
+
+@app.errorhandler(400)
+def bad_request(error):
+    desc = {
+        'title': '400 Bad Request',
+        'description': 'the server cannot or will not process the request due to something that is perceived to be a client error (e.g., malformed request syntax, invalid request message framing, or deceptive request routing).',
+        'fix': 'Please your request\'s syntax, framing, and routing.'
+    }
+    return render_template('error.html', error=desc), 400
+
+@app.errorhandler(500)
+def internal_error(error):
+    desc = {
+        'title': '500 Internal Server Error',
+        'description': 'The server encountered an unexpected condition, and prevented it from fulfilling the request...',
+        'fix': 'This is my fault.'
+    }
+    return render_template('500.html', error=desc), 500
