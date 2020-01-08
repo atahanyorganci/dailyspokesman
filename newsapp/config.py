511aa617d16434fe7cf59e631fa7bc17b3fe755e
Atahan Yorgancı <atahanyorganci@hotmail.com>
2020-01-09 00:26:38 +0300

    change news page routes from Turkish to English
    Change newspage routes from Turkish to English. Now display name, url
    for Sozcu, and newsapp api urls are different, and are stored in
    configuation object.

--- newsapp/config.py
+++ newsapp/config.py
@@ -7,12 +7,28 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(BASE_DIR, 'news.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    CATEGORIES = ['gundem', 'dunya', 'ekonomi']
+    CATEGORIES = [
+        {
+            'url': 'gundem',
+            'api': 'head',
+            'display': 'Headlines',
+        },
+        {
+            'url': 'dunya',
+            'api': 'world',
+            'display': 'World',
+        },
+        {
+            'url': 'ekonomi',
+            'api': 'econ',
+            'display': 'Economy',
+        },
+    ]
     SCRAPPER_BASE_URL = 'https://www.sozcu.com.tr/kategori/'
     CELERY_BROKER_URL = 'redis://localhost:6379/0'
     CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
-    CELERYBEAT_SCHEDULE = {f'update-news-{category}': {
+    CELERYBEAT_SCHEDULE = {f'update-news-{category["api"]}': {
         'task': 'newsapp.tasks.update_news',
         'schedule': 300.0,
-        'args': (category, )
+        'args': (category['url'], )
     } for category in CATEGORIES}
--- newsapp/routes.py
+++ newsapp/routes.py
@@ -1,5 +1,4 @@
 import math
-from pprint import pformat
 
 from flask import abort, render_template, url_for
 
@@ -17,22 +16,25 @@ def article_to_dict(article: Article) -> dict:
     }
 
 
-def pagination(category: str, number: int) -> dict:
-    count = Article.query.filter_by(category=category).count()
+def pagination(page: str, number: int) -> dict:
+    count = Article.query.filter_by(category=page).count()
     page_count = math.ceil(count / 5)
     out = {}
     if number == 1:
         out['prev'] = ''
-        out['labels'] = [(i, url_for('news', category=category, number=i)) for i in range(1, 4)]
-        out['next'] = url_for('news', category=category, number=number + 1)
+        out['labels'] = [(i, url_for('news', page=page, number=i))
+                         for i in range(1, 4)]
+        out['next'] = url_for('news', page=page, number=number + 1)
     elif number == page_count:
-        out['prev'] = url_for('news', category=category, number=number - 1)
-        out['labels'] = [(i, url_for('news', category=category, number=i)) for i in range(number - 2, number + 1)]
+        out['prev'] = url_for('news', page=page, number=number - 1)
+        out['labels'] = [(i, url_for('news', page=page, number=i))
+                         for i in range(number - 2, number + 1)]
         out['next'] = ''
     else:
-        out['prev'] = url_for('news', category=category, number=number-1)
-        out['labels'] = [(i, url_for('news', category=category, number=i)) for i in range(number - 1, number + 2)]
-        out['next'] = url_for('news', category=category, number=number+1)
+        out['prev'] = url_for('news', page=page, number=number-1)
+        out['labels'] = [(i, url_for('news', page=page, number=i))
+                         for i in range(number - 1, number + 2)]
+        out['next'] = url_for('news', page=page, number=number+1)
     return out
 
 
@@ -42,15 +44,17 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/<category>/<int:number>')
-def news(category, number):
-    if category not in app.config['CATEGORIES'] or number < 1:
+@app.route('/<page>/<int:number>')
+def news(page, number):
+    if number < 1:
         abort(404)
-    try:
-        articles = Article.query.filter_by(category=category).order_by(
-            Article.date).paginate(page=number, per_page=5).items
-        articles = [article_to_dict(article) for article in articles]
-        print(url_for('news', category='ekonomi', number=1))
-        return render_template('news.html', articles=articles, pagination=pagination(category, number))
-    except:
-        abort(404)
+    for category in app.config['CATEGORIES']:
+        if page == category['api']:
+            try:
+                articles = Article.query.filter_by(category=category['url']).order_by(
+                    Article.date).paginate(page=number, per_page=5).items
+                articles = [article_to_dict(article) for article in articles]
+                return render_template('news.html', articles=articles, pagination=pagination(category['url'], number))
+            except:
+                break
+    abort(404)
