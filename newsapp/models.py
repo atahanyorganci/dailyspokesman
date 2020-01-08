ca83198bb87b88528e607309e8c5fc15c620bd81
Atahan Yorgancı <atahanyorganci@hotmail.com>
2020-01-08 14:29:05 +0300

    implement Article model into the database

---
+++ newsapp/models.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+from newsapp import db
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    serialno = db.Column(db.Integer, nullable=False, unique=True)
+    category = db.Column(db.Integer, nullable=False)
+    title = db.Column(db.Text, nullable=False)
+    subtitle = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    link = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return f'<Article serial={self.serialno} category={self.category}>'
--- run.py
+++ run.py
@@ -1,4 +1,11 @@
-from newsapp import app
+from newsapp import app, db
+from newsapp.models import Article
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'Article': Article}
+
 
 if __name__ == "__main__":
     app.run()
