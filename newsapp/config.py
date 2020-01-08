ceacd5231c892e62516b34ef8ed869019768027e
Atahan Yorgancı <atahanyorganci@hotmail.com>
2020-01-08 14:25:51 +0300

    add SQL-Alchemy and Alembic for Flask
    Add SQL-Alchemy and Alembic configurations to Config object, and
    initalize instances in newsapp/__init__.py.

--- newsapp/config.py
+++ newsapp/config.py
@@ -4,3 +4,6 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'my-secret-key'
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(BASE_DIR, 'news.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
