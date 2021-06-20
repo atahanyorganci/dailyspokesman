ca808c6cf7dfe1b8f705d1ec8d2adc56b8cdd42b
Atahan Yorgancı <atahanyorganci@hotmail.com>
2021-06-21 00:29:44 +0300

    Implement cli command to scrape news from given category

--- newsapp/__init__.py
+++ newsapp/__init__.py
@@ -1,7 +1,10 @@
-from flask import Flask
+import click
+from flask import Flask, current_app
 
 from newsapp.config import Config
 from newsapp.models import db, migrate
+from newsapp.models.article import Article
+from worker.tasks import update_news
 
 
 def create_app(config_name=Config):
@@ -21,5 +24,20 @@ def create_app(config_name=Config):
     app.register_blueprint(errors)
     app.register_blueprint(main)
     app.register_blueprint(api)
+
+    @app.shell_context_processor
+    def shell_context():
+        return {'app': app, 'db': db, 'Article': Article}
+
+    @app.cli.command('scrape', help='Scrapes news from given category.')
+    @click.argument('category')
+    def scrape(category):
+        categories = current_app.config['CATEGORIES']
+        if category not in categories:
+            categories = ", ".join(key for key in categories.keys())
+            print(f'Unknown category, available categories: {categories}')
+            return
+
+        update_news(category)
 
     return app
